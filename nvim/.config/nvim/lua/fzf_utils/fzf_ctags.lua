bfeea2c9ebac91f0cbb870607b7e0ce305b1b4be
leisiji <xuelinye9@gmail.com>
2021-04-06 08:04:13 +0800

    [nvim] fix ctags function index

--- nvim/.config/nvim/lua/fzf_utils/fzf_ctags.lua
+++ nvim/.config/nvim/lua/fzf_utils/fzf_ctags.lua
@@ -6,15 +6,17 @@ local function get_ctags(file)
 	local cmd = string.format("ctags --output-format=json -u --fields=nzP -f- %s", file)
 	local res = fn.systemlist(cmd)
 	local funcs = {}
+	local count = 1
 
-	for idx,val in pairs(res) do
+	for _,val in pairs(res) do
 		local tag_json_obj = fn.json_decode(val)
 		if tag_json_obj["kind"] == "function" then
 			local pattern = tag_json_obj["pattern"]
 			local func_name = string.sub(pattern, 3, #pattern - 2)
 			local ln = tag_json_obj["line"]
 			local str = string.format("%s: %s", ln, func_name)
-			funcs[idx] = str
+			funcs[count] = str
+			count = count + 1
 		end
 	end
 
