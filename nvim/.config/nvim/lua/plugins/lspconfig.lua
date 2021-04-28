132bb69e5adc550b6a0ff7a7c4b4ea635cf0a84d
leisiji <xuelinye9@gmail.com>
2021-04-28 23:31:47 +0800

    update nvim-lsp-lua

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -14,33 +14,33 @@ local on_attach = function(client, _)
 	end
 end
 
+local default_cfg = { on_attach = on_attach }
+
 local function init_keymap()
 	local mapkey = vim.api.nvim_set_keymap
 	local opts = { noremap = true, silent = true }
 
-	--mapkey('n', '<C-j>', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-	--mapkey('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-	--mapkey('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-	mapkey('n', '<M-j>', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	mapkey('n', '<M-k>', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	mapkey('n', '<M-r>', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-	mapkey('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-	mapkey('n', '<C-h>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-	mapkey('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-	mapkey('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-	mapkey('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-	mapkey('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-	mapkey('n', '<space>a', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+	mapkey('n', '<M-j>', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
 
-	mapkey("n", "<leader><space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-	mapkey("v", "<leader><space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+	-- lspsaga
+	mapkey('n', '<M-r>', '<cmd>Lspsaga lsp_finder<cr>', opts)
+	mapkey('n', '<M-k>', "<cmd>Lspsaga hover_doc<cr>", opts)
+	mapkey('n', '<leader>j', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>", opts)
+	mapkey('n', '<space>rn', '<cmd>Lspsaga rename<cr>', opts)
+	mapkey('n', '<space>ca', '<cmd>Lspsaga code_action<cr>', opts)
+	mapkey('n', '<C-h>', '<cmd>Lspsaga signature_help<cr>', opts)
+
+	mapkey('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', opts)
+	mapkey('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', opts)
+	mapkey('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>', opts)
+	mapkey('n', '<space>a', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>', opts)
+
+	mapkey("n", "<leader><space>f", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
+	mapkey("v", "<leader><space>f", "<cmd>lua vim.lsp.buf.range_formatting()<cr>", opts)
 end
 
-local function lua_lsp()
-	local lspconfig = require('lspconfig')
-
+local function setup_lsp(lspconfig)
 	lspconfig.sumneko_lua.setup {
-		filetypes = { 'lua' };
 		cmd = {
 			'/usr/bin/lua-language-server',
 			'-E', '-e', 'LANG=EN',
@@ -58,32 +58,52 @@ local function lua_lsp()
 				},
 			},
 		};
-		on_attach = on_attach
+		on_attach = on_attach;
 	}
 
-	lspconfig.ccls.setup {
-		filetypes = { 'c', 'cpp' };
-		root_dir = lspconfig.util.root_pattern('compile_commands.json');
-	}
+	lspconfig.ccls.setup(default_cfg)
+	lspconfig.pyright.setup(default_cfg)
+	lspconfig.cmake.setup(default_cfg)
+	lspconfig.bashls.setup(default_cfg)
 end
 
-function M.init()
+local function setup_config(lspconfig)
+	local global_capabilities = vim.lsp.protocol.make_client_capabilities()
+	global_capabilities.textDocument.completion.completionItem.snippetSupport = true
+	global_capabilities.textDocument.completion.completionItem.resolveSupport = {
+		properties = {
+			'documentation',
+			'detail',
+			'additionalTextEdits',
+		}
+	}
+
+	lspconfig.util.default_config = vim.tbl_extend(
+		"force",
+		lspconfig.util.default_config,
+		{ capabilities = global_capabilities }
+	)
+end
+
+function M.init_lsp()
 	local guibg = COLORS.yellow
 	local guifg = COLORS.bg
 	local exec = vim.cmd
+	local lspconfig = require('lspconfig')
 
 	exec('hi LspReferenceRead guibg=' .. guibg .. ' guifg=' .. guifg)
 	exec('hi LspReferenceWrite guibg=' .. guibg .. ' guifg=' .. guifg)
 	exec('hi LspReferenceText guibg=' .. guibg .. ' guifg=' .. guifg)
 	init_keymap()
 
-	lua_lsp()
+	setup_config(lspconfig)
+	setup_lsp(lspconfig)
 end
 
 function M.completion_setup()
 	vim.g.completion_enable_snippet = 'snippets.nvim'
 	vim.g.completion_matching_smart_case = 1
-	vim.g.completion_trigger_keyword_length = 2
+	vim.g.completion_trigger_keyword_length = 3
 	vim.g.completion_timer_cycle = 500
 
 	require('completion').on_attach()
