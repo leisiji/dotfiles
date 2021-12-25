2c60449a83e9463eab9700d8e2ecf47c7977e06b
leisiji <xuelinye9@gmail.com>
2021-12-25 09:21:10 +0800

    update i3 config, gitsigns

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -27,13 +27,6 @@ end
 -- gitsigns
 function M.gitsigns()
   require('gitsigns').setup {
-    signs = {
-      add = {hl = 'GitGutterAdd', text = '+'},
-      change = {hl = 'GitGutterChange', text = '~'},
-      delete = { hl = 'GitGutterDelete', text = '_'},
-      topdelete = { hl = 'GitGutterDelete', text = '‾'},
-      changedelete = { hl = 'GitGutterChange', text = '~' }
-    },
     watch_index = { interval = 5000 },
     keymaps = {
       noremap = true, buffer = true,
@@ -42,12 +35,6 @@ function M.gitsigns()
       ['n <leader><leader>b'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
     }
   }
-  vim.cmd[[
-    hi DiffAdd    guibg=#26332c guifg=NONE
-    hi DiffChange guibg=#273842 guifg=NONE
-    hi DiffDelete guibg=#572E33 guifg=NONE
-    hi DiffText   guibg=#314753 guifg=NONE
-  ]]
 end
 
 function M.filetype()
