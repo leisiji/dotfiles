607f67315834eef4cd0113e8063dea5047b741b8
leisiji <xuelinye9@gmail.com>
2022-03-02 16:52:57 +0800

    [nvim] update nvim-tree and calltree config

--- nvim/.config/nvim/lua/plugins/calltree.lua
+++ nvim/.config/nvim/lua/plugins/calltree.lua
@@ -1,34 +1,30 @@
 local M = {}
 
-function M.mapkeys()
-  local opts = { noremap = true, silent = true }
-  local keymaps = {
-    {'l', 'expand_calltree()'},
-    {'h', 'collapse_calltree()'},
-    {'<cr>', 'jump_calltree()'},
-    {'<C-t>', "jump_calltree('tab')"},
-    {'<M-k>', 'hover_calltree()'},
-  }
-  for _, v in ipairs(keymaps) do
-    local rhs = [[:lua require('litee.calltree').]] .. v[2] .. '<cr>'
-    vim.api.nvim_buf_set_keymap(0, 'n', v[1], rhs, opts)
-  end
-end
-
 function M.config()
   require('litee.lib').setup({
-    panel = { orientation = "left", panel_size  = 80 }
+    panel = { orientation = "left", panel_size  = 80 },
   })
-  require('litee.calltree').setup({})
+  require('litee.calltree').setup({
+    keymaps = {
+      expand = "l",
+      collapse = "h",
+      collapse_all = "zM",
+      jump = "<CR>",
+      jump_split = "<C-s>",
+      jump_vsplit = "<C-v>",
+      jump_tab = "<C-t>",
+      hover = "<M-k>",
+      details = "d",
+      close = "q",
+      close_panel_pop_out = "<Esc>",
+      hide = "H",
+      switch = "S",
+      focus = "f"
+    },
+  })
   vim.cmd[[
     command! CallTreeI lua vim.lsp.buf.incoming_calls()
     command! CallTreeO lua vim.lsp.buf.outgoing_calls()
-  ]]
-  vim.cmd[[
-    augroup my_calltree
-      autocmd!
-      au Filetype calltree lua require('plugins.calltree').mapkeys()
-    augroup END
   ]]
 end
 
--- nvim/.config/nvim/lua/plugins/nvim_tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim_tree.lua
@@ -2,37 +2,41 @@ local M = {}
 local original_width = nil
 
 function M.setup()
-  vim.g.nvim_tree_gitignore = 0
   vim.g.nvim_tree_git_hl = 0
-  vim.g.nvim_tree_show_icons = { git = 0, folders = 1, folder_arrows = 1, }
-  local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+  vim.g.nvim_tree_show_icons = { git = 0, folders = 1, folder_arrows = 1 }
+  local tree_cb = require("nvim-tree.config").nvim_tree_callback
   local list = {
-    { key = "l",     cb = tree_cb("edit") },
-    { key = "h",     cb = tree_cb("close_node") },
+    { key = "l", cb = tree_cb("edit") },
+    { key = "h", cb = tree_cb("close_node") },
     { key = "<C-]>", cb = tree_cb("cd") },
     { key = "<C-[>", cb = tree_cb("dir_up") },
     { key = "<C-v>", cb = tree_cb("vsplit") },
     { key = "<C-x>", cb = tree_cb("split") },
     { key = "<C-t>", cb = tree_cb("tabnew") },
-    { key = ".",     cb = tree_cb("toggle_dotfiles") },
-    { key = "R",     cb = tree_cb("refresh") },
-    { key = "a",     cb = tree_cb("create") },
-    { key = "D",     cb = tree_cb("remove") },
-    { key = "r",     cb = tree_cb("rename") },
-    { key = "x",     cb = tree_cb("cut") },
-    { key = "c",     cb = tree_cb("copy") },
-    { key = "p",     cb = tree_cb("paste") },
-    { key = "y",     cb = tree_cb("copy_name") },
-    { key = "Y",     cb = tree_cb("copy_path") },
-    { key = "gy",    cb = tree_cb("copy_absolute_path") },
-    { key = "q",     cb = tree_cb("close") },
-    { key = "z",     cb = [[:lua require('plugins.nvim_tree').resize()<cr>]] },
+    { key = ".", cb = tree_cb("toggle_dotfiles") },
+    { key = "R", cb = tree_cb("refresh") },
+    { key = "a", cb = tree_cb("create") },
+    { key = "D", cb = tree_cb("remove") },
+    { key = "r", cb = tree_cb("rename") },
+    { key = "x", cb = tree_cb("cut") },
+    { key = "c", cb = tree_cb("copy") },
+    { key = "p", cb = tree_cb("paste") },
+    { key = "y", cb = tree_cb("copy_name") },
+    { key = "Y", cb = tree_cb("copy_path") },
+    { key = "gy", cb = tree_cb("copy_absolute_path") },
+    { key = "q", cb = tree_cb("close") },
+    { key = "z", cb = [[:lua require('plugins.nvim_tree').resize()<cr>]] },
   }
-  require'nvim-tree'.setup { view = { mappings = { custom_only = false, list = list} } }
+  require("nvim-tree").setup({
+    view = { mappings = { custom_only = false, list = list } },
+    git = {
+      enable = false,
+    },
+  })
 end
 
 function M.resize()
-  local w = require'nvim-tree.view'.View.width
+  local w = require("nvim-tree.view").View.width
   if original_width == nil then
     original_width = w
   end
@@ -41,9 +45,9 @@ function M.resize()
   else
     w = w + 50
   end
-  require'nvim-tree.view'.View.width = w
-  vim.cmd('NvimTreeClose')
-  vim.cmd('NvimTreeToggle')
+  require("nvim-tree.view").View.width = w
+  vim.cmd("NvimTreeClose")
+  vim.cmd("NvimTreeToggle")
 end
 
 return M
