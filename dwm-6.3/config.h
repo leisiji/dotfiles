a874f24bef211c3e214dfd146e48e2547ab26862
leisiji <xuelinye9@gmail.com>
2022-03-27 14:47:55 +0800

    add date; change font

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "CaskaydiaCove Nerd Font Mono:size=10" };
+static const char *fonts[]          = { "CaskaydiaCove Nerd Font:size=12" };
 static const char purple[]                   = "#9a7ae1";
 static const char black[]                 = "#232831";
 static const char bg_normal[]             = "#1C1F24";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "\uf489", "\uf7ae", "\ufc58", "\uf1b7", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
