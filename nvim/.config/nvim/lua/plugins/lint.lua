a174c2c3651fd1c03b20b2c1fb88d20a513be22b
leisiji <xuelinye9@gmail.com>
2022-08-14 17:26:07 +0800

    [nvim] import markdownlint

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -8,6 +8,12 @@ function M.config()
   }
   lint.linters_by_ft = linters
 
+  local md = require('lint.linters.markdownlint')
+  md.args = {
+    "-c",
+    vim.fn.stdpath("config") .. "/.markdownlint.jsonc"
+  }
+
   local group = "nvim_lint"
   local a = vim.api
   a.nvim_create_augroup(group, { clear = true })
@@ -18,7 +24,7 @@ function M.config()
       lint.try_lint()
     end,
   })
-  vim.diagnostic.config({virtual_text = false})
+  vim.diagnostic.config({ virtual_text = false })
 end
 
 return M
