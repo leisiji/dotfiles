7a560ccc5f80f9cc8ae96619b1ef7390c8cce701
leisiji <xuelinye9@gmail.com>
2022-09-24 10:06:58 +0800

    update wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -17,6 +17,22 @@ return {
       event = { Up = { streak=2, button="Left" } },
       mods = "NONE",
       action = wezterm.action{ CopyTo="ClipboardAndPrimarySelection" }
-    }
-  }
+    },
+    {
+      event={Up={streak=1, button="Left"}},
+      mods="NONE",
+      action=wezterm.action{CompleteSelectionOrOpenLinkAtMouseCursor="Clipboard"},
+    },
+  },
+  window_decorations = "NONE",
+  window_frame = {
+      border_left_width = '0.1cell',
+      border_right_width = '0.1cell',
+      border_bottom_height = '0.1cell',
+      border_top_height = '0.1cell',
+      border_left_color = 'grey',
+      border_right_color = 'grey',
+      border_bottom_color = 'grey',
+      border_top_color = 'grey',
+  },
 }
