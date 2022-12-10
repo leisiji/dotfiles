610c6ccf64c6d22c455c29fdba0f0cb70b116a39
leisiji <xuelinye9@gmail.com>
2022-12-10 21:33:12 +0800

    [nvim] only use lsp semantic tokens on large file

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -32,13 +32,13 @@ local function treesitter()
     ensure_installed = "all",
     highlight = {
       enable = true,
-      -- disable = function(_, buf)
-      --   local max_filesize = 100 * 1024 -- 100 KB
-      --   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      --   if ok and stats and stats.size > max_filesize then
-      --     return true
-      --   end
-      -- end,
+      disable = function(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
     },
     indent = {
       enable = true,
