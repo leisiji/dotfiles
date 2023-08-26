2ce501807f0d79270baf296e1723e0093d2aaf30
leisiji <xuelinye9@gmail.com>
2023-08-26 09:32:50 +0800

    fix femaco failed in termux
    As os.tmpname() return fail in TERMUX, use another path

--- nvim/.config/nvim/lua/plugins/femaco.lua
+++ nvim/.config/nvim/lua/plugins/femaco.lua
@@ -1,11 +1,17 @@
 local M = {}
 
 function M.config()
-  require("femaco").setup({
-    ensure_newline = function ()
+  local config = {
+    ensure_newline = function()
       return true
+    end,
+  }
+  if vim.env.TERMUX_APP_PID then
+    config.create_tmp_filepath = function(filetype)
+      return vim.fn.stdpath("cache") .. "femaco"
     end
-  })
+  end
+  require("femaco").setup(config)
 end
 
 return M
