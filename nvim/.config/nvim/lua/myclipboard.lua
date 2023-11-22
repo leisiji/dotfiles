441843d6150165d6540ff26a7526375f0d8c2bb2
leisiji <xuelinye9@gmail.com>
2023-11-22 09:58:21 +0800

    update nvim config

--- nvim/.config/nvim/lua/myclipboard.lua
+++ nvim/.config/nvim/lua/myclipboard.lua
@@ -1,5 +1,5 @@
 vim.g.clipboard = {
   name = "OSC 52",
-  copy = { ["*"] = { "tmux", "load-buffer", "-" }, ["+"] = require("vim.clipboard.osc52").copy },
-  paste = { ["*"] = { "tmux", "save-buffer", "-" }, ["+"] = require("vim.clipboard.osc52").paste },
+  copy = { ["*"] = { "tmux", "load-buffer", "-" }, ["+"] = require('vim.ui.clipboard.osc52').copy('+') },
+  paste = { ["*"] = { "tmux", "save-buffer", "-" }, ["+"] = require('vim.ui.clipboard.osc52').paste('+') },
 }
