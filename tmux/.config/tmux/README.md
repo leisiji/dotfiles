7ed3ecaf77f0d07a87023fc4bd93759df33c0802
leisiji <2265215145@qq.com>
2025-04-09 08:12:10 +0800

    add tmux floatx

---
+++ tmux/.config/tmux/README.md
@@ -0,0 +1,4 @@
+# plugins
+
+- [tmux-floax](https://github.com/omerxx/tmux-floax)
+- [tmux-fuzzback](https://github.com/roosta/tmux-fuzzback)
--- tmux/.config/tmux/scripts/fzf-tmux-scrollback.sh
+++ tmux/.config/tmux/scripts/fzf-tmux-scrollback.sh
@@ -2,7 +2,7 @@
 tmux_fuzzyback() {
     local content=$(tmux capture-pane -p -S -)
     local fzf_tmux=$(dirname $0)/fzf-tmux
-    local line=$(echo "${content}" | awk '{print NR  " " $s}' | ${fzf_tmux} -p)
+    local line=$(echo "${content}" | awk '{print NR  " " $s}' | ${fzf_tmux} -w 80% -h 60% -p)
     line=$(echo ${line} | awk '{print $1}')
     if [[ ! -z ${line} ]]; then
         local rows=$(echo "${content}" | wc -l)
