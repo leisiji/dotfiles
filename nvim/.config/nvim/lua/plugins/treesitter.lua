a72519eeccc42400e5e48c3ddca331547bba5e20
leisiji <2265215145@qq.com>
2025-10-16 10:54:27 +0800

    udpate nvim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -38,7 +38,7 @@ function M.config()
       enable = true,
       additional_vim_regex_highlighting = false,
       disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
+        local max_filesize = 500 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
@@ -67,11 +67,9 @@ function M.config()
         set_jumps = true,
         goto_next_end = {
           ["]f"] = "@function.outer",
-          ["]i"] = "@conditional.inner",
         },
         goto_previous_start = {
           ["[f"] = "@function.outer",
-          ["[i"] = "@conditional.inner",
         },
       },
     },
@@ -79,6 +77,24 @@ function M.config()
   vim.keymap.set({ "x", "o" }, "ik", function()
     require("nvim-treesitter.textobjects.select").select_textobject("@block.inner", "textobjects")
   end, { desc = "Treesitter: inside block" })
+
+  vim.keymap.set("n", "[c", function()
+    local shared = require("nvim-treesitter.textobjects.shared")
+    local _, _, node = shared.textobject_at_point("@conditional.outer", "textobjects")
+    if node then
+      local start_row, start_col = node:start()
+      vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col })
+    end
+  end, { desc = "Jump to current conditional start" })
+
+  vim.keymap.set("n", "]c", function()
+    local shared = require("nvim-treesitter.textobjects.shared")
+    local _, _, node = shared.textobject_at_point("@conditional.outer", "textobjects")
+    if node then
+      local end_row, end_col = node:end_()
+      vim.api.nvim_win_set_cursor(0, { end_row + 1, end_col })
+    end
+  end, { desc = "Jump to current conditional end" })
 end
 
 return M
