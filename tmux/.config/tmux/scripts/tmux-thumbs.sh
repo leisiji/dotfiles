1621008af17072a95431f56076d2fda15509ec3a
leisiji <2265215145@qq.com>
2025-12-01 09:13:59 +0800

    update tmux-thumbs

--- tmux/.config/tmux/scripts/tmux-thumbs.sh
+++ tmux/.config/tmux/scripts/tmux-thumbs.sh
@@ -1,6 +1,10 @@
-res=$(tmux list-panes -F \"#{pane_id}:#{pane_height}\")
+res=$(tmux list-panes -F "#{pane_id}:#{pane_height}")
 id=${res%:*}
 pane_height=${res#*:}
-thumbs_pane_id=$(tmux new-window -P -F "#{pane_id}" -d -n "[thumbs]" "tmux capture-pane -t ${id} -p | tail -n ${pane_height} | thumbs -f '%U:%H' -t /tmp/thumbs-last; tmux swap-pane -t ${id}; tmux wait-for -S thumbs-finished")
-echo ${id} ${thumbs_pane_id}
+tmp_dir="/tmp"
+if [ -n ${TMPDIR} ]; then
+	tmp_dir=${TMPDIR};
+fi
+tmp_file=${tmp_dir}/thumbs-last
+thumbs_pane_id=$(tmux new-window -P -F "#{pane_id}" -d -n "[thumbs]" "tmux capture-pane -t ${id} -p | tail -n ${pane_height} | thumbs --hint-fg-color red -f '%U:%H' -t ${tmp_file}; tmux swap-pane -t ${id}; tmux wait-for -S thumbs-finished; tmux set-buffer -w $(thumbs_out=$(cat ${TMPDIR}/thumbs-last);thumbs_target=${thumbs_out#*:};echo ${thumbs_target})")
 tmux swap-pane -d -s ${id} -t ${thumbs_pane_id}
