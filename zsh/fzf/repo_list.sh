0c77fb5e632c3cc48525d1a47cf63196aba3552b
leisiji <2265215145@qq.com>
2026-03-03 10:46:01 +0800

    refine repo_list.sh

--- zsh/fzf/repo_list.sh
+++ zsh/fzf/repo_list.sh
@@ -1,4 +1,10 @@
-for i in $(repo list 2>/dev/null | awk '{print $1}'); do
-	[[ ${1} == "directory" ]] && echo ${2}/${i};
-	fd -t ${1} -a --base-directory ${2}/${i};
-done
+if [[ ${1} == "directory" ]]; then
+	fd -t d --no-ignore -a --base-directory ${2}
+else
+	for i in $(repo list 2>/dev/null | awk '{print $1}'); do
+		git_dir=${2}/${i}
+		if git -C ${git_dir} rev-parse --git-dir > /dev/null 2>&1; then
+			git -C ${git_dir} ls-files | sed "s|^|${git_dir}/|"
+		fi
+	done
+fi
