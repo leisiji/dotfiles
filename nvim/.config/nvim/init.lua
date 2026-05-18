0023936840019176e4c058c407149205e0446581
leisiji <2265215145@qq.com>
2026-05-18 10:42:29 +0800

    disable mouse in tmux

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -238,7 +238,6 @@ init_plugins_keymaps()
 -- vim.o.mouse not work: https://github.com/neovim/neovim/issues/28992
 vim.cmd([[
   set list lcs=tab:→\ ,trail:·
-  set mouse="nv"
 ]])
 
 require("vim._core.ui2").enable({})
