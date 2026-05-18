ebe552228b81ed52fa75ec8bba6f468d09eb646c
leisiji <2265215145@qq.com>
2026-05-18 19:47:41 +0800

    update fish

--- fish/.config/fish/functions/gp.fish
+++ fish/.config/fish/functions/gp.fish
@@ -1,4 +1,20 @@
 function gp --description 'Git push current branch'
     set -l branch (git branch --show-current)
-    git push origin $branch:$branch
+
+    # Detect repo workspace by walking up looking for .repo/
+    set -l dir (git rev-parse --show-toplevel 2>/dev/null)
+    set -l is_repo false
+    while test -n "$dir" -a "$dir" != "/"
+        if test -d "$dir/.repo"
+            set is_repo true
+            break
+        end
+        set dir (dirname $dir)
+    end
+
+    if test "$is_repo" = true
+        git push origin $branch:refs/for/$branch
+    else
+        git push origin $branch:$branch
+    end
 end
