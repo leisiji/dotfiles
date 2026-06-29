c58d25420d723cde9df4f82129cac13e5e456051
leisiji <2265215145@qq.com>
2026-06-29 15:39:58 +0800

    add maskefile-lsp

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -25,6 +25,17 @@ function M.lsp_config()
   vim.lsp.inlay_hint.enable(true)
   local group = vim.api.nvim_create_augroup("my.lsp", { clear = true })
 
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    pattern = "make",
+    callback = function()
+      vim.lsp.start({
+        name = "makefile-lsp",
+        cmd = { "makefile-lsp" },
+      })
+    end,
+  })
+
   vim.api.nvim_create_autocmd("LspProgress", {
     group = group,
     callback = function(ev)
