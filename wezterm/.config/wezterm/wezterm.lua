9a58e270d5e493fda1730c20ad796e89efda74b8
leisiji <2265215145@qq.com>
2026-09-09 11:38:17 +0800

    update config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -27,6 +27,7 @@ end
 config.leader = { key = "n", mods = "ALT", timeout_milliseconds = 2000 }
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab  = true
 
 config.colors = {
   tab_bar = {
